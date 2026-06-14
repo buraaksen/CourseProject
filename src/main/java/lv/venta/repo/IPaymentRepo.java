e3dd5f52542c0858f64b3e8a1e6efc84eecf8812
Burak Şen <buraaksen@outlook.com>
2026-06-14 13:54:43 +0300

    some minimal changes added

--- src/main/java/lv/venta/repo/IPaymentRepo.java
+++ src/main/java/lv/venta/repo/IPaymentRepo.java
@@ -1,11 +1,10 @@
 package lv.venta.repo;
 
 import java.util.ArrayList;
+
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import lv.venta.model.Payment;
 import lv.venta.model.PaymentMethod;
@@ -14,8 +13,8 @@ public interface IPaymentRepo extends CrudRepository<Payment, Integer> {
 
 	Optional<Payment> findByReservation_IdRES(int reservationId);
 
-	@Query("SELECT p FROM Payment p WHERE p.isSuccesfull = :isSuccessful")
-	ArrayList<Payment> findBySuccessStatus(@Param("isSuccessful") boolean isSuccessful);
+
+	ArrayList<Payment> findBySuccessStatus( boolean isSuccessful);
 
 	ArrayList<Payment> findByPayment(PaymentMethod method);
 
